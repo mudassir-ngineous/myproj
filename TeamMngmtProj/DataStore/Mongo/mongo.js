16cbca78bb701a48ec6fb09bcc9aa553e9db49ef
YaserHashmi <yaser.hashmi313@gmail.com>
2016-12-16 23:59:59 +0530

    added insert function in mongo

--- TeamMngmtProj/DataStore/Mongo/mongo.js
+++ TeamMngmtProj/DataStore/Mongo/mongo.js
@@ -1,6 +1,22 @@
-function insert(doc){
-
-}
-
-
-exports.insert=insert;
+function insert(doc) {
+    var MongoClient = require(mongodb).MongoClient;
+    var url = "mongodb://localhost:27017/luqman";
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            console.log("not connected to server");
+        } else {
+            console.log("Connected correctly to server");
+        }
+        var collection = db.collection("docs");
+        collection.insert(doc, function (err, result) {
+            if (err) {
+                console.log("doc not inserted");
+            } else {
+                console.log("inserted 3 doc");
+            }
+            console.dir(docs);
+            db.close();
+        });
+    });
+};
+exports.insert = insert;
